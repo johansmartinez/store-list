28b2ca66174207dc02debbec253a3f6ead07cf9d
johansmartinez <johan.martinez.dev@gmail.com>
2022-01-26 15:51:55 -0500

    agregando el ciclo de vida

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,19 +1,39 @@
-import { Component, OnInit,Input,Output, EventEmitter } from '@angular/core';
+import { Component, OnInit,Input,Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit,OnChanges,AfterViewInit,OnDestroy {
 
   @Input() img:string='';
   @Output() imgLoaded= new EventEmitter<string>();
 
-  constructor() { }
+  constructor() { 
+    //no async y solo 1 vez
+  }
+  ngOnDestroy(): void {
+    //cuando se elimina el componente
+  }
+
+  ngAfterViewInit(): void {
+    //depués del render
+    //manejan los hijos
+  }
+
+  ngOnChanges(){
+    //después del constructor y antes del render
+    // cambios en inputs
+    //muchas veces
+  }
 
   ngOnInit(): void {
+    //antes del render y después del Onchanges
+    //se pueden hacer async ó fetc
   }
+
+
   onError(){
 
   }
